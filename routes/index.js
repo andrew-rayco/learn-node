103a835648b5dab146949a5534221892d8f5ae7a
Andrew <andy@andycondon.com>
2017-08-03 15:57:12 +1200

    be ready to start part 8

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  console.log(req.query)
-  res.render('hello', {
-    name: 'Andy',
-    dog: req.query.dog,
-    title: 'I love food'
-  });
-});
+const storeController = require('../controllers/storeController')
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse)
-})
+// Do work here
+router.get('/', storeController.homePage)
 
 module.exports = router;
