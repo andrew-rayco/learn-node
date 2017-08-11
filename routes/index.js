104cf9bfb0f2ae420d4e5bbc7e3c28d9627ce4eb
Andrew <andy@andycondon.com>
2017-08-11 15:57:07 +1200

    error handling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const storeController = require('../controllers/storeController')
+const { catchErrors } = require('../handlers/errorHandlers')
+
 
 // Do work here
 router.get('/', storeController.homePage)
